1eeb6f70cbc83720c683212a954bff2ef3b654d7
AtlastDeepLearning <89887461+AtlastDeepLearning@users.noreply.github.com>
2023-10-19 14:39:38 +0800

    Added the necessary code for program.cpp and users.h

---
+++ program.cpp
@@ -0,0 +1,75 @@
+#include "MyBank_Login.h"
+#include "MyBank_Signup.h"
+#include "MyBank_MainForm.h"
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+User^ AuthenticateUser()
+{
+    while (true)
+    {
+        User^ user = nullptr;
+
+        MyBank::MyBank_Login loginForm;
+        loginForm.ShowDialog();
+
+        if (loginForm.switchToRegister)
+        {
+            MyBank::MyBank_Signup registerForm;
+            registerForm.ShowDialog();
+
+            if (registerForm.switchToLogin)
+            {
+                continue;
+            }
+            else
+            {
+                return registerForm.user;
+            }
+        }
+        else
+        {
+            return loginForm.user;
+        }
+    }
+}
+
+void HandleUserSession()
+{
+    User^ user = nullptr;
+    while (true)
+    {
+        user = AuthenticateUser();
+
+        if (user != nullptr)
+        {
+            MyBank::MyBank_MainForm mainForm(user);
+            mainForm.ShowDialog();
+
+            if (mainForm.switchBackToLogin)
+            {
+                continue;
+            }
+            else
+            {
+                break; // Exiting the program
+            }
+        }
+        else
+        {
+            break; // No user, exit program
+        }
+    }
+}
+
+int main(array<String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    HandleUserSession();
+
+    return 0;
+}
--- users.h
+++ users.h
@@ -1,1 +1,14 @@
 #pragma once
+
+using namespace System;
+
+public ref class User {
+public:
+	int id;
+	String^ fname;
+	String^ lname;
+	String^ username;
+	String^ accNumber;
+	String^ password;
+	Decimal balance;
+};
